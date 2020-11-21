c52c98aba82ee5cd4179a5f3152c6337b1f8471b
toniyansyah4 <pembuatanwebsite61@gmail.com>
2020-11-21 13:14:51 +0700

    FIX

--- Login Authentication/halaman/otentikasi.js
+++ Login Authentication/halaman/otentikasi.js
@@ -1,20 +1,16 @@
 var time = new Date();
-console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()
-);
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
 
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username == "rizqi12" && password == "123") {
+    if (username == "toni" && password == "123467") {
         // alert("selamat anda berhasil login!!!");
         window.location = "halaman/halaman.html";
         return false;
-    }
-    else {
+    } else {
         alert('Anda harus mengisi data dengan benar!');
         return false;
     }
 }
-
-
